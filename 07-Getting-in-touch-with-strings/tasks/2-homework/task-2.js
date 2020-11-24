af17041d886ba816529cbb96ceaecb5e02d2d042
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-11-25 03:29:45 +0400

    feat(7-Getting-in-touch-with-strings): improved task-2

--- 07-Getting-in-touch-with-strings/tasks/2-homework/task-2.js
+++ 07-Getting-in-touch-with-strings/tasks/2-homework/task-2.js
@@ -2,10 +2,14 @@ function extractCurrencyValue(string) {
   if (typeof string !== 'string')
     throw new Error("Parameter must be a string");
 
-  if (string.charCodeAt(0) >= "1".charCodeAt(0) && string.charCodeAt() <= "9".charCodeAt(0))
-    return parseInt(string);
-  else
+  if (string.length >= 1 && string.charCodeAt() >= "1".charCodeAt() && string.charCodeAt() <= "9".charCodeAt()) {
+    if (string.split("").every(o => o.charCodeAt() >= "0".charCodeAt() && o.charCodeAt() <= "9".charCodeAt()))
+      return parseInt(string);
+  }
+  else if (string.length !== 1 && string.substr(1).split("").every(o => o.charCodeAt() >= "0".charCodeAt() && o.charCodeAt() <= "9".charCodeAt()))
     return parseInt(string.substr(1));
+
+  throw new Error("Parameter has not a proper value");
 }
 
-console.log(extractCurrencyValue('$120'));
+console.log(extractCurrencyValue("$120"));
