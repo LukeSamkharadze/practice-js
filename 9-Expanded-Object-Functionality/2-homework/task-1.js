82a76b7f9b3b6a07d496d3d0f181d71fd897a821
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-11-27 02:39:01 +0400

    feat(9-Expanded-Object-Functionality): finished homework

---
+++ 9-Expanded-Object-Functionality/2-homework/task-1.js
@@ -0,0 +1,37 @@
+Object.defineProperty(Object.prototype, "mergeDeepRight", {
+  value: function (o) {
+    Object.getOwnPropertyNames(o).forEach(p => {
+      if (Array.isArray(this[p]) && Array.isArray(o[p]))
+        this[p] = o[p].concat(this[p]);
+      else if (typeof this[p] === 'object' && typeof o[p] === 'object')
+        this[p].mergeDeepRight(o[p]);
+      else
+        this[p] = o[p];
+    })
+  },
+});
+
+let data = {
+  name: 'fred',
+  age: 10,
+  contact: {
+    email: 'moo@example.com',
+    meta: {
+      verified: true,
+      tags: ['important']
+    }
+  }
+};
+
+data.mergeDeepRight({
+  age: 40,
+  contact: {
+    email: 'baa@example.com',
+    favorite: true,
+    meta: {
+      tags: ['vip']
+    }
+  }
+});
+
+console.log(data);
